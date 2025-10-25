db091ab061cb0031ffd0a5387602605b369ca17e
koppala srikar <koppalashanmukhasrikar@gmail.com>
2025-10-25 11:32:10 +0530

    added

--- backend/db_setup.py
+++ backend/db_setup.py
@@ -2,7 +2,7 @@
 # ------------
 
 from sqlalchemy import create_engine, Column, String, Integer, Text, DateTime, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 from config import DB_PATH
